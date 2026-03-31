5f9acb8499fec7c052f88c72640acdf441475c8d
Felipe-de-Carvalho-Andrade <felipea07112006@gmail.com>
2026-03-31 15:42:56 -0300

    Implementação do Scanner básico com apenas uma Thread

---
+++ CyberSecurity/PortScanner/scanner.py
@@ -0,0 +1,14 @@
+import socket
+
+target = "scanme.nmap.org"
+
+for port in range(1, 1025):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    result = sock.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[OPEN] Porta {port}")
+    
+    sock.close()
